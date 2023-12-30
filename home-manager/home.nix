a2f1e4e733a53aa996092f12ed29e821f8a917c7
kafurakami <zoldyck1179@gmail.com>
2023-12-31 00:27:25 +0530

    flaky future

---
+++ home-manager/home.nix
@@ -0,0 +1,154 @@
+{ 
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+
+  # For other home manager modules
+  imports = [
+  ];
+  
+  nixpkgs = {
+    overlays = [
+    ];
+
+    config = {
+      # For unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  # Home Directory
+  home = {
+    username = "murphodinger";
+    home-directory = "/home/murphodinfger";
+  };
+
+  home-packages = with pkgs; [
+  # Utilites
+  gparted
+  steam-run
+  libsForQt5.yakuake
+  libsForQt5.gwenview
+  cloudflare-warp
+  mpv
+  qbittorrent
+
+  # CLI utilites
+  curl
+  fzf
+  lsd
+  btop
+  aria2
+  ranger
+  glibc
+  gcc13
+  libgccjit
+  wl-clipboard
+
+  # VPN
+  cloudflare-warp
+  mullvad-vpn
+
+  # Editors
+  vscode
+
+  # Gaming
+  bottles-unwrapped
+  winetricks
+  wineWowPackages.stable
+
+  # Browsers
+  firefox
+  brave
+
+  # Entertainment
+  stremio
+  netflix
+  spotify
+
+  # Fonts
+  (nerdfonts.override { fonts = [ "Iosevka" "NerdFontsSymbolsOnly" ]; })
+  jetbrains-mono
+
+  ];
+    
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    BROWSER = "firefox";
+    TERMINAL = "kitty";
+    # EDITOR = "emacs";
+  };
+
+  fonts.fontconfig.enable = true;
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # ZSH Config
+  programs.zsh = {
+  enable = true;
+
+  shellAliases = {
+    ls = "lsd";
+    ll = "lsd -l";
+  };
+
+  plugins = [
+    {
+      name = "zsh-nix-shell";
+      file = "nix-shell.plugin.zsh";
+      src = pkgs.fetchFromGitHub {
+      owner = "chisui";
+        repo = "zsh-nix-shell";
+        rev = "v0.7.0";
+        sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+      };
+    }
+  ];
+
+  initExtra = ''
+    source $HOME/.p10k.zsh
+    eval "$(direnv hook zsh)"
+  '';
+  
+  oh-my-zsh = {
+    enable = true;
+    plugins = [ "git" "fzf" "direnv" "ssh-agent" ]; 
+    custom = "$HOME/.omz-custom/";
+    theme = "powerlevel10k/powerlevel10k";
+  };
+
+  syntaxHighlighting.enable = true; 
+  enableAutosuggestions = true;
+  };
+
+  # Kitty Config
+  programs.kitty.enable = true;
+  #programs.kitty.shellIntegration.enableFishIntegration = true;
+  programs.kitty.shellIntegration.enableZshIntegration = true;
+  xdg.configFile."kitty/kitty.conf".source = ./kitty/kitty.conf;
+  xdg.configFile."kitty/colors.conf".source = ./kitty/colors.conf;
+
+  # LSD Config
+  xdg.configFile."lsd/config.yaml".source = ./lsd/config.yaml;
+
+
+  # Direnv Config
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,238 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ 
+  inputs,
+  lib,
+  config, 
+  pkgs, 
+  ... 
+}: {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix comamnds consistent as well!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc = 
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.consoleLogLevel = 3
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "bgrt";
+
+  networking.hostName = "arasaka"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    
+    extraPackages = with pkgs; [
+    	vaapiVdpau
+	libvdpau-va-gl
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  services.thermald.enable = false;
+
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+    };
+  };
+
+  services.power-profiles-daemon.enable = false;
+  
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # AMD GPU
+   services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
+  
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    gwenview
+    kate
+    khelpcenter
+    plasma-browser-integration
+    print-manager
+  ];
+
+  programs.dconf.enable = true;
+
+
+  # Fonts
+#  fonts.fonts = with pkgs; [
+#    (nerdfonts.override { fonts = [ "Iosevka" ]; })
+#    jetbrains-mono
+#  ];
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # PipeWire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.murphodinger = {
+    isNormalUser = true;
+    initialPassword = "correcthorsebatterystaple";
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     firefox
+  #     tree
+  #   ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim
+    plymouth
+    wget
+    exfat
+    keyd
+    git
+    curl
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  services.supergfxd.enable = true;
+  services.asusd.enable = true;
+  services.asusd.enableUserService = true;
+
+  # Keyd
+  # Create systemd service
+  systemd.services.keyd = {
+    description = "key remapping daemon";
+    requires = [ "local-fs.target" ];
+    after = [ "local-fs.target" ];
+    wantedBy = [ "sysinit.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.keyd}/bin/keyd";
+      # Restart = "on-failure";
+    };
+  };
+
+  # Setting up Keyd config
+  environment.etc."keyd/default.conf".text = ''
+    [ids]
+    *
+
+    [shift]
+
+    [main]
+    capslock = overload(control, esc)
+    rightalt = -
+  '';
+
+  services.openvpn.servers = {
+     arasaka = { config = '' config /home/murphodinger/.vpn/arasaka.ovpn ''; autoStart = false; updateResolvConf = true; };
+  };
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
+
